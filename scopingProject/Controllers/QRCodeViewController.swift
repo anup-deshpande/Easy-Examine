55a219bd576a24dd19cae6e235f8c0492fbaf7cb
Anup Deshpande <deshpande.anup24@gmail.com>
2019-11-09 17:09:46 -0500

    Add login screen

---
+++ scopingProject/Controllers/QRCodeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QRCodeViewController.swift
+//  scopingProject
+//
+//  Created by Anup Deshpande on 11/9/19.
+//  Copyright © 2019 Anup Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class QRCodeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- scopingProject/Controllers/loginViewController.swift
+++ scopingProject/Controllers/loginViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class loginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func scanQRCodeTapped(_ sender: UIButton) {
+        
+        self.performSegue(withIdentifier: "loginToQRCodeSegue", sender: nil)
+        
+    }
+    
 }
 
