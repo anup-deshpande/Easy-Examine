cdd154c99f15fcbd9438938405841ce046722b5a
Adwait Tathe <adwaittathe@Adwaits-MacBook-Pro.local>
2019-11-09 17:41:08 -0500

    QR CODE

--- scopingProject/Controllers/QRCodeViewController.swift
+++ scopingProject/Controllers/QRCodeViewController.swift
@@ -7,24 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
-class QRCodeViewController: UIViewController {
+class QRCodeViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
 
+{
+    @IBOutlet weak var square: UIImageView!
+    var video = AVCaptureVideoPreviewLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let session = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
+            print("Failed to get the camera device")
+            return
+            
+        }
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            session.addInput(input)
+        }
+        catch{
+            print("ERROR")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubviewToFront(square)
+        
+        session.startRunning()
+        
 
-        // Do any additional setup after loading the view.
+    }
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+         if metadataObjects != nil && metadataObjects.count != 0
+         {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject
+            {
+                if object.type == AVMetadataObject.ObjectType.qr
+                {
+                    let alert = UIAlertController(title: "QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Retake", style: .default, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    
+                    present(alert,animated: true,completion: nil)
+                }
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    */
 
 }
